19d752ab5f6c41d3a08e554d1bd0019698e2a21e
Murilo Araujo <muernandes@icloud.com>
2020-09-26 21:24:00 -0300

    Feat(youtube): Add youtube controller with download list funcionality

---
+++ MAYoutubeDownload/Controllers/DownloadController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using YouTubeSearch;
+
+namespace MAYoutubeDownload.Controllers
+{
+    public class DownloadController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Video(string url)
+        {
+            try
+            {
+                IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(url, false);
+
+                return Ok(videoInfos);
+            }
+            catch (Exception ex) {
+                return NotFound(ex.Message);
+            }
+           
+        }
+    }
+}
